f7eb150bdb6c0d6fad338e3be2d5e0909bd69945
ilya <lystsov.ilya@gmail.com>
2024-02-26 19:49:53 +0300

    add tongue param

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -7,3 +7,4 @@ parser = argparse.ArgumentParser(
 parser.add_argument("text", type=str, help="The message the cow will say.", nargs="?")
 parser.add_argument("-e", help="The cow's eyes.", default="oo")
 parser.add_argument("-f", help="The cow file to use.", default="default")
+parser.add_argument("-T", help="The cow's tongue.", default="  ")
