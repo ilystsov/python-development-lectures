7d1c261f65402d1cd691cf39a189fe0cc88b7acc
ilya <lystsov.ilya@gmail.com>
2024-02-26 19:49:24 +0300

    add cowfile param

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -6,3 +6,4 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument("text", type=str, help="The message the cow will say.", nargs="?")
 parser.add_argument("-e", help="The cow's eyes.", default="oo")
+parser.add_argument("-f", help="The cow file to use.", default="default")
