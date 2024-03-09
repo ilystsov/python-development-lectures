2d7cd928be04308eec9a1bc8b652bf1ffbc1afb3
ilya <lystsov.ilya@gmail.com>
2024-03-09 21:57:10 +0300

    modify list_cows command

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -3,18 +3,20 @@ import shlex
 import cowsay
 import cmd
 
+
 class CowsayShell(cmd.Cmd):
     prompt = '(cowsay)'
     intro = "Python version of the classic cowsay program. Type help or ? to list commands.\n"
 
     def do_list_cows(self, arg):
-        """List all cow characters."""
+        """List all cow characters.\nUSAGE: list_cows [path/to/file]"""
 
         args = shlex.split(arg)
         if len(args) == 1:
             print(cowsay.list_cows(arg if arg else cowsay.COW_PEN))
-
-
+        else:
+            print('Invalid arguments!')
+            return
 
 
 if __name__ == '__main__':
