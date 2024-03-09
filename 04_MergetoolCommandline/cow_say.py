eebda030f3c7451a903307e8dcbc948478b98fed
ilya <lystsov.ilya@gmail.com>
2024-03-09 22:15:09 +0300

    add cowsay command

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -33,6 +33,24 @@ class CowsayShell(cmd.Cmd):
             wrap_text = bool(args[2])
         print(cowsay.make_bubble(text=text, width=width, wrap_text=wrap_text))
 
+    def do_cowsay(self, arg):
+        """Print cow saying message.\nUSAGE: cowsay message [cow [eyes [tongue]]]"""
+        args = shlex.split(arg)
+        if len(args) == 0 or len(args) >= 5:
+            print('Invalid arguments!')
+            return
+        message = args[0]
+        cow = 'default'
+        eyes = cowsay.Option.eyes
+        tongue = cowsay.Option.tongue
+        if len(args) >= 2:
+            cow = args[1]
+        if len(args) >= 3:
+            eyes = args[2]
+        if len(args) >= 4:
+            tongue = args[3]
+        print(cowsay.cowsay(message=message, cow=cow, eyes=eyes, tongue=tongue))
+
 
 if __name__ == '__main__':
     CowsayShell().cmdloop()
