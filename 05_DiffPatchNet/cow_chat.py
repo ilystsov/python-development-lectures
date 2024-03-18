f0b7ec80b96f7fa74e9542c90ee5eb14610c7779
ilya <lystsov.ilya@gmail.com>
2024-03-18 21:47:49 +0300

    add login command

--- 05_DiffPatchNet/cow_chat.py
+++ 05_DiffPatchNet/cow_chat.py
@@ -1,32 +1,55 @@
 #!/usr/bin/env python3
 import asyncio
+import cowsay
 
-clients = {}
+clients: dict[str, asyncio.Queue] = {}
+
+def handle_login(me: str | None, name: str) -> tuple[bool, str]:
+    if me is None:
+        if name not in cowsay.list_cows():
+            return False, 'There is no such cow name.'
+        if name in clients:
+            return False, 'This name has already been taken by someone else.'
+        clients[name] = asyncio.Queue()
+        return True, 'Logged in successfully!'
+    else:
+        return False, 'You have already been logged in. You have to quit first to log in again.'
+
 
 async def chat(reader, writer):
-    me = "{}:{}".format(*writer.get_extra_info('peername'))
-    print(me)
-    clients[me] = asyncio.Queue()
+    me = None
     send = asyncio.create_task(reader.readline())
-    receive = asyncio.create_task(clients[me].get())
     while not reader.at_eof():
-        done, pending = await asyncio.wait([send, receive], return_when=asyncio.FIRST_COMPLETED)
-        for q in done:
-            if q is send:
+        if me is None:
+            done, _ = await asyncio.wait([send], return_when=asyncio.FIRST_COMPLETED)
+        else:
+            done, pending = await asyncio.wait([send, receive], return_when=asyncio.FIRST_COMPLETED)
+
+        for task in done:
+            if task is send:
+                input_text = task.result().decode().strip()
+                match input_text.split():
+                    case ['login', name]:
+                        logged_in_flag, response = handle_login(me, name)
+                        if logged_in_flag:
+                            me = name
+                            receive = asyncio.create_task(clients[me].get())
+                        writer.write(f"{response}\n".encode())
+                        await writer.drain()
+
                 send = asyncio.create_task(reader.readline())
-                for out in clients.values():
-                    if out is not clients[me]:
-                        await out.put(f"{me} {q.result().decode().strip()}")
-            elif q is receive:
+            elif task is receive:
                 receive = asyncio.create_task(clients[me].get())
-                writer.write(f"{q.result()}\n".encode())
+                writer.write(f"{task.result()}\n".encode())
                 await writer.drain()
     send.cancel()
     receive.cancel()
     print(me, "DONE")
-    del clients[me]
+    if me:
+        del clients[me]
     writer.close()
     await writer.wait_closed()
+
 
 async def main():
     server = await asyncio.start_server(chat, '0.0.0.0', 1337)
